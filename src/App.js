a53c1fac4f3d534dae665486e18cb1cd92305fad
maria <airamfdezs@gmail.com>
2022-04-29 18:28:43 +0200

    Variable created. Changes in class naming

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Header from "./components/header/Header";
-
+import Nav from "./components/nav/Nav";
 function App() {
   return (
     <div className="App">
       <Header />
+      <Nav />
     </div>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,47 +3,49 @@ import React from 'react';
 import {FaFacebook, FaInstagram, FaShoppingCart, FaUser}from "react-icons/fa";
 import {FaTwitter} from "react-icons/fa";
 import {FaSearch} from "react-icons/fa";
-
 import './scss/header.scss';
+
 //import logo from './logo.png'; 
 
 
 const Header = () => {
     
   return (
+    <>
     <header>
-        <section className='header__socials'>
-            <ul className='header-socials__ul'>
+          <section className='header__socials'>
+          <ul className='header_socials__ul'>
 
-                <li className='socials-ul__li'>
-                <a href='www.instagram.com' className='socials-icon__anchor'><FaFacebook /></a>
+              <li className='socials_ul__li'>
+                  <a href='www.instagram.com' className='socials_icon__anchor'><FaFacebook /></a>
 
-                </li>
-                <li className='socials-ul__li'>
-                    <a href='www.instagram.com' className='socials-icon__anchor'><FaInstagram /></a>
-                </li>
-                <li className='socials-ul__li'>
-                    <a href='www.twitter.com' className='socials-icon__anchor'><FaTwitter /></a>
-                </li>
-            </ul>
-        </section>
-        <section className='header__main'>
-            <section className='header-main__search'>
-                <FaSearch className='main-search__icon'/>
-                <input type='text' className='main-search__input' placeholder='Buscador'/>
-            </section>
-            <section className='header-main__logo'>
-                <figure className='main-logo__img'>
-                    <img src='' alt="Logo" />
-                </figure>
-            </section>
-            <section className='header-main__userArea'>
-                <FaUser className='userArea__icon'/>
-                <FaShoppingCart className='userArea__icon'/>
-            </section>
-        </section>
+              </li>
+              <li className='socials_ul__li'>
+                  <a href='www.instagram.com' className='socials_icon__anchor'><FaInstagram /></a>
+              </li>
+              <li className='socials_ul__li'>
+                  <a href='www.twitter.com' className='socials_icon__anchor'><FaTwitter /></a>
+              </li>
+          </ul>
+      </section><section className='header__main'>
+              <section className='header_main__search'>
+                  <FaSearch className='main_search__icon'/>
+                  <input type='text' className='main_search__input' placeholder='Buscador' />
+              </section>
+              <section className='header_main__logo'>
+                  <figure className='main_logo__img'>
+                      <img src='' alt="Logo" />
+                  </figure>
+              </section>
+              <section className='header_main__userArea'>
+                  <FaUser className='userArea__icon'/>
+                  <FaShoppingCart className='userArea__icon'/>
+              </section>
+          </section>
+         
 
     </header>
+    </>
   )
 }
 
