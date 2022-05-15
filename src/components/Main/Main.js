e0c1a5d8e2058e422612225bbce777673d7f0c67
maria <airamfdezs@gmail.com>
2022-05-15 18:28:24 +0200

    Background home page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Slider from '../Slider/Slider';
+import './scss/main.scss';
+import bg_dm from '../../exports/img/bg_dm.jpeg';
 
 
 const Main = () => {
   return (
-    <div style={{height:'500px'}}>
-       <Slider />
-    </div>
-
+    <>
+    <main className='main__container'>
+      <div className='main__img' style={{backgroundImage:`url(${bg_dm})`, height:'900px', width:'100%'}}>
+        <p className='img__text'> Development made fashion </p>
+      </div>
+      <Slider />
+    </main>
+    </>
   )
 }
 
